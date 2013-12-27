68fbefc3f455d226845ed57c3b11195a73b106b4
PikaDotus <loganh@pobox.com>
2013-12-26 22:34:02 -0800

    finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name					"Logan Howard"
-		email					"loganh@pobox.com"
-		password				"testpassword"
-		password_confirmation	"testpassword"
+		sequence(:name)		{ |n| "Person #{n}" }
+		sequence(:email)	{ |n| "person_#{n}@example.com" }
+		password 'foobardesu'
+		password_confirmation 'foobardesu'
+
+		factory :admin do
+			admin true
+		end
 	end
 end
