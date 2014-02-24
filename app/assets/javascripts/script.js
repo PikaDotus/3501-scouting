9afc49a0d460a28395a88ba00cd00d45c5ee386c
PikaDotus <loganh@pobox.com>
2014-02-23 18:45:08 -0800

    darn missed a (

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,7 +3,7 @@ WebFontConfig = {
 };
 (function() {
   var wf = document.createElement('script');
-  wf.src = ('https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
   wf.type = 'text/javascript';
   wf.async = 'true';
   var s = document.getElementsByTagName('script')[0];
