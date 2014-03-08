70d55a8e51f67fccd871947f432704aaa7f151a3
PikaDotus <loganh@pobox.com>
2014-03-08 08:26:04 -0800

    make team display a bit nicer

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,14 @@
 module MatchesHelper
+
+	# Returns the string of the match type
+	def match_type_to_words(match_type)
+		case match_type
+			when 0
+				return 'Practice'
+			when 1
+				return 'Qual'
+			when 2
+				return 'Elim'
+		end
+	end
 end
