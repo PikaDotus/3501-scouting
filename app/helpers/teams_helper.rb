01115d05346711fef6c072b12b42b29d277e88e8
PikaDotus <loganh@pobox.com>
2014-03-08 21:36:41 -0800

    change img size

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -3,7 +3,7 @@ module TeamsHelper
 	# Returns the img of the team's robot
 	def pic_for(team)
 		if team.pic_url
-			image_tag(team.pic_url, alt: team.number, class: 'team-pic', width: 300)
+			image_tag(team.pic_url, alt: team.number, class: 'team-pic', width: 400)
 		end
 	end
 end
