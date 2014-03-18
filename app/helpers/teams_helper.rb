ae61978b139e7bbe7301636428cf85a0917f2241
PikaDotus <loganh@pobox.com>
2014-03-17 17:31:26 -0700

    round in team display

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -6,4 +6,47 @@ module TeamsHelper
 			image_tag(team.pic_url, alt: team.number, class: 'team-pic', width: 400)
 		end
 	end
+
+	def note_for(team, email)
+		eligible_matches = @matches.where(scouter_email: 'loganh@pobox.com')
+
+		offset = rand(eligible_matches.count)
+		final_note = eligible_matches.first(offset: offset).notes.random
+	end
+
+	def stat_for(team, stat)
+		require 'open-uri'
+
+		doc = Nokogiri::HTML(open('http://www2.usfirst.org/2014comp/events/cama/rankings.html'))
+
+		# there are 9 extra/other <tr> tags
+		num_teams = doc.css('tr').count  - 9
+
+		curTeam = 5
+		team_data = -1
+
+		begin
+			team_data = doc.css('tr')[curTeam].text.chop.split("\n")
+
+			team_data.each_with_index do |data, index|
+				team_data[index] = data.to_i
+			end
+		end while (team_number != team.number) and (curTeam <= num_teams)
+
+		case stat
+			when 'team_number'
+				team_data[1]
+			when 'assist_score'
+				team_data[3]
+			when 'auto_score'
+				team_data[4]
+			when 'truss_and_catch'
+				team_data[5]
+			when 'teleop_score'
+				team_data[6]
+			when 'matches'
+				team_data[9]
+			else "?"
+		end
+	end
 end
