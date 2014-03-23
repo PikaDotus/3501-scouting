13c483cc68fd3825edbce5bc64473704481066dd
PikaDotus <loganh@pobox.com>
2014-03-23 15:19:40 -0700

    got test chart to work

---
+++ app/assets/javascripts/charts.js
@@ -0,0 +1,34 @@
+function createChart() {
+	var data = {
+		labels : ["Eating","Drinking","Sleeping","Designing","Coding","Partying","Running"],
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				data : [65,59,90,81,56,55,40]
+			},
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,1)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				data : [28,48,40,19,96,27,100]
+			}
+		]
+	}
+
+	var options = {
+		scaleFontFamily: "'Helvetica'",
+		scaleLineColor: 'rgba(255, 255, 255, .5)',
+		angleLineColor: 'rgba(255, 255, 255, .5)',
+		pointLabelFontColor : "#ffffff",
+		scaleFontColor : "#ffffff"
+	}
+
+	// get the context with jQuery
+	var context = $("#myChart").get(0).getContext('2d');
+	// get first returned node
+	var myNewChart = new Chart(context).Radar(data, options);
+}
